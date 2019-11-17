52830537d7fff1bdd0d713a1523e571d279a302e
Wesley Moses <wesmoses@gmail.com>
2019-11-17 15:14:44 +0200

    adds firebase & config

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import * as firebase from 'firebase'
 import React from 'react'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
@@ -5,6 +6,19 @@ import Projects from './screens/Projects'
 import Issues from './screens/Issues'
 import Details from './screens/Details'
 import CreateProject from './screens/Create-project'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBewYA7cN0XjSSGcD8C_X5A6C35zVrKOCA",
+  authDomain: "react-native-bugtracker.firebaseapp.com",
+  databaseURL: "https://react-native-bugtracker.firebaseio.com",
+  projectId: "react-native-bugtracker",
+  storageBucket: "react-native-bugtracker.appspot.com",
+  messagingSenderId: "769567494057",
+  appId: "1:769567494057:web:985589cf0343dba09c3eb8"
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
 const AppNavigator = createStackNavigator({
   Projects,
