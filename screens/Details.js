c3567e24b855f62dc11deebbe8a937662b9e0982
Wesley Moses <wesmoses@gmail.com>
2019-11-25 18:59:03 +0200

    details screen uses dao and properly toggles completed

--- screens/Details.js
+++ screens/Details.js
@@ -2,7 +2,7 @@ import * as firebase from 'firebase'
 import React, { useState, useEffect } from 'react'
 import { View } from 'react-native'
 import { Text, Button, CheckBox } from 'react-native-elements'
-import { getIssue, updateIssue } from '../models/IssueDAO'
+import { getIssue, updateIssue, removeIssue } from '../models/IssueDAO'
 import { getCategory } from '../models/ProjectDAO'
 
 DetailsScreen.navigationOptions = {
@@ -11,49 +11,36 @@ DetailsScreen.navigationOptions = {
 
 export default function DetailsScreen({ route, navigation }) {
   const [loading, setLoading] = useState(false)
-  const [issueCompleted, setIssueCompleted] = useState(false)
-  const [category, setCategory] = useState(null)
   const [issue, setIssue] = useState({})
+  const [completedOn, setCompletedOn] = useState(false)
+  const [category, setCategory] = useState(null)
   const { id } = navigation.getParam('issue')
   const project = navigation.getParam('project')
 
   useEffect(() => {
     setLoading(true)
-    Promise.resolve(getIssue(project.id, id)).then(issueObject => {
-      if (!issueObject) {
-        navigation.navigate('Issues')
-        return setLoading(false)
-      } else {
-        setIssue(issueObject)
-        issueObject.completedOn && setIssueCompleted(true)
-        return setLoading(false)
-      }
+    Promise.resolve(getIssue(project.id, id)).then(issue => {
+      setIssue(issue)
+      setCompletedOn(issue.completedOn || false)
+      Promise.resolve(getCategory(project.id, issue.category)).then(category => {
+        setCategory(category)
+        setLoading(false)
+      })
     })
   }, [])
 
   useEffect(() => {
-    Promise.resolve(getCategory(project.id, issue.category)).then(category =>
-      setCategory(category)
-    )
-
-    console.log(issue.completedOn)
-  }, [issue])
-
-  setComplete = complete => {
     setLoading(true)
-    setIssueCompleted(complete)
-    const completedOn = complete
-      ? new Date()
-      : null
-    Promise.resolve(updateIssue({ id, projectId: project.id, completedOn}))
-      .then(() => setLoading(false))
-  }
-
-  removeIssue = () => {
-    setLoading(true)
-    const removeIssue = firebase.database().ref(`issues/${project.id}/${id}`);
-    removeIssue.remove(() => {
+    Promise.resolve(updateIssue({ ...issue, completedOn })).then(() =>
       setLoading(false)
+    )
+  }, [completedOn])
+
+  this.removeIssue = () => {
+    setLoading(true)
+    Promise.resolve(removeIssue(issue)).then(() => {
+      setLoading(false)
+      navigation.navigate('Issues')
     })
   }
 
@@ -68,11 +55,11 @@ export default function DetailsScreen({ route, navigation }) {
     <Text>{category ? category.name : 'No category'}</Text>
     <Text style={{ display: issue.dueDate ? 'flex' : 'none' }}>Due on {issue.dueDate}</Text>
     <Text>Importance: {issue.importance}</Text>
-    <Text style={{ display: issueCompleted ? 'flex' : 'none' }}>Completed on {issue.completedOn}</Text>
+    <Text style={{ display: completedOn ? 'flex' : 'none' }}>Completed on {completedOn ? completedOn.toString() : ''}</Text>
     <CheckBox
-      title='Complete'
-      checked={issueCompleted}
-      onPress={() => setComplete(!issueCompleted)}
+      title='Completed'
+      checked={completedOn ? true : false}
+      onPress={() => setCompletedOn(completedOn ? false : new Date())}
     />
     <Button title="Remove" onPress={() => this.removeIssue()} />
     <Button title="Edit" onPress={() => navigation.navigate('AddEditIssue', { project, issue: { id, ...issue } })} />
